5bd93bb02ca6789e1a90f18fa281406aacb42e12
Norbert Pozsonyi <norbert.pozsonyi@gmail.com>
2015-11-23 15:44:03 +0100

    python 'fizzbuzz' game v2.0

--- week-03/day1/00-python-test/07_fizzbuzz.py
+++ week-03/day1/00-python-test/07_fizzbuzz.py
@@ -1,12 +1,12 @@
 n=0
 while n < 100:
-    if n % 3 == 0 or n % 5 == 0:
-        if n % 3 != 0:
-            print('buzz')
-        elif n % 5 != 0:
-            print('fizz')
-        else:
+    if n % 3 == 0:
+        if n % 5 == 0:
             print('fizzbuzz')
+        else:
+            print('fizz')
+    elif n % 5 == 0:
+        print('buzz')
     else:
-        print (n)
+        print(n)
     n += 1
