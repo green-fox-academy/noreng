23f7de066ebb22efca9868d12dac4daeb1f02d98
Norbert Pozsonyi <norbert.pozsonyi@gmail.com>
2015-11-23 13:57:14 +0100

    make the first boolean

--- week-03/day1/00-python-test/01_expression.py
+++ week-03/day1/00-python-test/01_expression.py
@@ -1,2 +1,24 @@
-a=23
-print(a)
+# Positive or negative
+a = 1
+
+if a == 0:
+    print('It is zero')
+elif a > 0:
+    print('It is positive')
+    print('It is really positive')
+    print('I mean it!')
+else:
+    print('It is negative')
+    print('It is really negative')
+    print('I mean it!')
+
+
+# Even or odd
+n = 0
+
+if n % 2 == 0:
+    print('even')
+elif n % 2 == 1:
+    print('odd')
+else:
+    print('not an integer')
