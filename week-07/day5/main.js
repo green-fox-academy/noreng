3133d025d05a0d2035b9d03910a6f8d059c0eee1
Norbert Pozsonyi <norbert.pozsonyi@gmail.com>
2016-01-08 00:34:50 +0100

    js gallery: create init() function

--- week-07/day5/main.js
+++ week-07/day5/main.js
@@ -1,17 +1,28 @@
 'use strict';
 
-var currentIndex = 0;
-
-var images = [
+var imageSources = [
   'http://lorempixel.com/400/200/technics/1',
   'http://lorempixel.com/400/200/technics/2',
   'http://lorempixel.com/400/200/technics/3',
   'http://lorempixel.com/400/200/technics/4'
 ];
 
-var bigPicture = document.querySelector('.gallery');
-var buttons = document.querySelector('.buttons');
-var thumbnails = document.querySelector('.thumbnails');
+var currentIndex, image, buttons, thumbnails;
+
+init();
+
+function init() {
+  currentIndex = 0;
+  initDomElements();
+  generateThumbnails();
+  setCurrentThumbnailActive();
+}
+
+function initDomElements() {
+  image = document.querySelector('.gallery');
+  buttons = document.querySelector('.buttons');
+  thumbnails = document.querySelector('.thumbnails');
+}
 
 buttons.addEventListener('click', function(event) {
   setIndexByDirection(event.target.id);
@@ -39,7 +50,7 @@ function setIndexBySelectedThumbnail(thumbnail) {
 }
 
 function handleIndexOnEnds(index) {
-  var end = images.length - 1;
+  var end = imageSources.length - 1;
   if (currentIndex < 0) {
     currentIndex = end;
   } else if (end < currentIndex) {
@@ -50,7 +61,7 @@ function handleIndexOnEnds(index) {
 function displayCurrentImage() {
   removeActiveClassFromThumbnails();
   setCurrentThumbnailActive();
-  bigPicture.setAttribute('src', images[currentIndex]);
+  image.setAttribute('src', imageSources[currentIndex]);
 }
 
 function removeActiveClassFromThumbnails() {
@@ -63,15 +74,14 @@ function setCurrentThumbnailActive() {
   current.classList.add('active');
 }
 
-(function generateThumbnails(src) {
+function generateThumbnails() {
   var thumbnail;
-  images.forEach(function(src) {
+  imageSources.forEach(function(src) {
     thumbnail = document.createElement('img');
     thumbnail.setAttribute('src', src );
     thumbnails.appendChild(thumbnail);
   });
-  setCurrentThumbnailActive()
-})()
+}
 
 function getIndexOfElement(element) {
   var siblings = element.parentNode.childNodes;
